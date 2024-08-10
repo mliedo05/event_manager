d68b9cdf9e2e862ab05f225beac85a7f0210e9dd
mliedo05 <m.liendo@wapii.com>
2024-08-09 21:23:55 -0400

    add migration table event

---
+++ db/migrate/20240810011650_create_events.rb
@@ -0,0 +1,19 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.timestamp :start_time
+      t.timestamp :end_time
+      t.boolean :is_paid, default: false
+      t.boolean :is_remote, default: false
+      t.integer :status, default: 0
+      t.string :address
+      t.integer :capacity
+      t.text :internal_note
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
