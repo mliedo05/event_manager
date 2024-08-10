beb12271161ab8b18115127bbcd18183b6336744
mliedo05 <m.liendo@wapii.com>
2024-08-09 20:54:09 -0400

    running rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # root to: "home#index"
 end
