df86e816d3dc47c9c266ff86a1ecaaa1c7adebbb
Angel Campo <angelcampo@MacBook-Air-de-Angel.local>
2024-08-15 17:37:39 -0400

    creating only admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def only_admin!
+    redirect_to root_path unless current_user.admin?
+    flash[:alert] = 'No tienes permitido realizar esta acción'
+  end
 end
